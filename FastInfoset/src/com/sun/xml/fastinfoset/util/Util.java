0ea9bfdd012c08d540275e2952fe558b3b95d4c7
joehw <joehw@localhost>
2005-01-23 18:10:43 +0000

    Add getEventTypeString
    svn path=/trunk/; revision=53

--- FastInfoset/src/com/sun/xml/fastinfoset/util/Util.java
+++ FastInfoset/src/com/sun/xml/fastinfoset/util/Util.java
@@ -38,6 +38,7 @@
 
 
 package com.sun.xml.fastinfoset.util;
+import javax.xml.stream.XMLStreamConstants;
 
 public class Util {
     
@@ -52,5 +53,33 @@ public class Util {
         else
             return true;
     } 
+    
+    public final static String getEventTypeString(int eventType) {
+        switch (eventType){
+            case XMLStreamConstants.START_ELEMENT:
+                return "START_ELEMENT";
+            case XMLStreamConstants.END_ELEMENT:
+                return "END_ELEMENT";
+            case XMLStreamConstants.PROCESSING_INSTRUCTION:
+                return "PROCESSING_INSTRUCTION";
+            case XMLStreamConstants.CHARACTERS:
+                return "CHARACTERS";
+            case XMLStreamConstants.COMMENT:
+                return "COMMENT";
+            case XMLStreamConstants.START_DOCUMENT:
+                return "START_DOCUMENT";
+            case XMLStreamConstants.END_DOCUMENT:
+                return "END_DOCUMENT";
+            case XMLStreamConstants.ENTITY_REFERENCE:
+                return "ENTITY_REFERENCE";
+            case XMLStreamConstants.ATTRIBUTE:
+                return "ATTRIBUTE";
+            case XMLStreamConstants.DTD:
+                return "DTD";
+            case XMLStreamConstants.CDATA:
+                return "CDATA";
+        }
+        return "UNKNOWN_EVENT_TYPE";
+    }
     
 }
