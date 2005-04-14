ddf69cf6358a9e85c8870e093e86fc819d7b3293
sandoz <sandoz@localhost>
2005-04-14 17:10:53 +0000

    no message
    svn path=/trunk/; revision=203

---
+++ FastInfoset/src/org/jvnet/fastinfoset/RestrictedAlphabet.java
@@ -0,0 +1,51 @@
+/*
+ * Fast Infoset ver. 0.1 software ("Software")
+ * 
+ * Copyright, 2004-2005 Sun Microsystems, Inc. All Rights Reserved. 
+ * 
+ * Software is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at:
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations.
+ * 
+ *    Sun supports and benefits from the global community of open source
+ * developers, and thanks the community for its important contributions and
+ * open standards-based technology, which Sun has adopted into many of its
+ * products.
+ * 
+ *    Please note that portions of Software may be provided with notices and
+ * open source licenses from such communities and third parties that govern the
+ * use of those portions, and any licenses granted hereunder do not alter any
+ * rights and obligations you may have under such open source licenses,
+ * however, the disclaimer of warranty and limitation of liability provisions
+ * in this License will apply to all Software in this distribution.
+ * 
+ *    You acknowledge that the Software is not designed, licensed or intended
+ * for use in the design, construction, operation or maintenance of any nuclear
+ * facility.
+ *
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ *
+ */ 
+
+package org.jvnet.fastinfoset;
+
+public final class RestrictedAlphabet {
+    
+    public static final String NUMERIC_CHARACTERS = "0123456789-+.E ";
+    
+    public static final int NUMERIC_CHARACTERS_INDEX = 0;
+    
+    public static final String DATE_TIME_CHARACTERS = "0123456789-:TZ ";
+
+    public static final int DATE_TIME_CHARACTERS_INDEX = 1;
+    
+}
