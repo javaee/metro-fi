1e358dc9d35b02fd3495c7912979f5d50c7fbd17
joehw <joehw@localhost>
2005-05-23 23:29:42 +0000

    test all
    svn path=/trunk/; revision=274

---
+++ RoundTripTests/bin/allRoundtripTests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+roundtripTest.sh saxroundtrip_rtt
+roundtripTest.sh staxroundtrip_rtt
+roundtripTest.sh domroundtrip_rtt
+roundtripTest.sh domsaxroundtrip_rtt
+roundtripTest.sh saxstaxdiff_rtt
