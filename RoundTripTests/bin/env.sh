fbaaafaf56b7d1370ea89194d2294f5336781eaa
joehw <joehw@localhost>
2005-05-23 17:53:46 +0000

    FastInfoset RoundTripTests initial checkin
    svn path=/trunk/; revision=271

---
+++ RoundTripTests/bin/env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export FI_HOME=/projects/fws/cvsroot/fi/FastInfoset
+export FIRTT_HOME=/projects/fws/cvsroot/fi/FastInfosetRTCT
+export PATH=/usr/local/bin:/usr/bin:/usr/X11R6/bin:/bin:/usr/games:/opt/kde3/bin:/opt/local/bin:/usr/local/java/bin:/usr/local/ant/bin:/projects/fws/cvsroot/fi/FastInfosetRTCT/bin:/projects/fws/cvsroot/fi/FastInfoset/bin
+
---
+++ RoundTripTests/bin/roundtripTest.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+    echo Usage: trountripTest [roundtrip test name]
+    exit
+fi
+
+
+handle_dir() {
+    
+    cd "$1"
+    for file in *
+    do
+        if [ -d "$file" ]; then
+            #echo $file is directory
+            handle_dir "$file" $2
+        else
+            #echo $file is file
+            handle_file "$file" $2
+        fi
+    done 
+    cd ".."
+}     
+
+handle_file() {
+    file=$1
+    cmd=$2
+    ext=${file##*.}
+    if [ $ext = "xml" ]; then
+        #echo $file is an xml file
+        $cmd $file
+    #else
+        #echo $file is not an xml file
+    fi
+}
+
+
+cmd=$1
+
+for file in *
+do
+    if [ -d "$file" ]; then
+        #echo $file is directory
+        handle_dir "$file" $cmd
+    else
+        #echo $file is file, perform Xerces test
+        handle_file "$file" $cmd
+    fi
+done 
+
+removeEmptyDir.sh
