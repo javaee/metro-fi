397ca6920efdcb212e672f3e868bdbb9d60bc743
nobody <nobody@localhost>
2005-05-31 22:38:50 +0000

    This commit was manufactured by cvs2svn to create tag 'FAST_INFOSET_JWSDP_1_6_08'.
    svn path=/tags/FAST_INFOSET_JWSDP_1_6_08/; revision=287
    
    
    Former-commit-id: 66dfbcf9a0f5d683af971129c6a4ba3fb7ffadea

--- RoundTripTests/bin/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export FI_HOME=/projects/fws/cvsroot/fi/FastInfoset
-export FIRTT_HOME=/projects/fws/cvsroot/fi/RoundTripTests
-export FIRTT_DATA=${FIRTT_HOME}/data
-export PATH=/usr/local/bin:/usr/bin:/usr/X11R6/bin:/bin:/opt/kde3/bin:/opt/local/bin:/usr/local/java/bin:/usr/local/ant/bin:${FIRTT_HOME}/bin:${FI_HOME}/bin
-
