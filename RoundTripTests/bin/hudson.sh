69e606b38ead868753fbf946053c9661267c2fe5
joehw <joehw@localhost>
2006-04-19 05:48:26 +0000

    script for hudson
    svn path=/trunk/; revision=561
    
    
    Former-commit-id: c595f5ea84c81315928876016e12b846553b2bf7

---
+++ RoundTripTests/bin/hudson.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+#set env:
+#FIRTT_HOME 
+#TIMESTAMP=`date '+%m%d%y_%H%M'`
+
+export FI_HOME=${FIRTT_HOME}/../FastInfoset
+export FIRTT_DATA=${FIRTT_HOME}/data
+export RESULT_HOME=${FIRTT_HOME}/report
+
+
+#.bashrc on fi.eastsource /projects/fws/.bashrc
+#source ${HOME}/.bashrc
+export PATH=${FIRTT_HOME}/bin:${FI_HOME}/bin:.:$PATH
+
+# Get sources from java.net, compile FastInfoset and RoundTripTests
+ant -f ${FIRTT_HOME}/build-without-nb.xml dist
+
+chmod 755 ${FIRTT_HOME}/bin/*
+chmod 755 ${FI_HOME}/bin/*
+
+cd $FIRTT_DATA/xmlconf
+REPORT_TS=xmlts_${TIMESTAMP}.html
+${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_TS}
+
+cd ${FIRTT_DATA}/XBC
+REPORT_XBC=xbc_${TIMESTAMP}.html
+${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_XBC}
+
+mv ${FIRTT_DATA}/${REPORT_TS} ${RESULT_HOME}
+mv ${FIRTT_DATA}/${REPORT_XBC} ${RESULT_HOME}
+
+LOGFILE=${FIRTT_HOME}/report/roundtrip_${TIMESTAMP}.log
+
+echo ""                                                                 >> ${LOGFILE}
+echo "RountTripTests results"                                  >> ${LOGFILE}
+echo "-------------------------------------------"                      >> ${LOGFILE}
+echo "Test results for well-formed files from XML TS:"         >>${LOGFILE}
+echo "${REPORT_TS}" >> ${LOGFILE}
+echo "-------------------------------------------"                      >> ${LOGFILE}
+echo "Test results for files from XBC test corpus:"         >>${LOGFILE}
+echo "${REPORT_XBC}" >> ${LOGFILE}
