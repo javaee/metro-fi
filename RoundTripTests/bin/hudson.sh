faa5ce66aeb20a16da3592ab5c3de1e4aff0f484
joehw <joehw@localhost>
2006-04-29 05:11:19 +0000

    debug hudson setup: xmlts.html and xbc.html under /report not archieved.
    svn path=/trunk/; revision=566
    
    
    Former-commit-id: 7bdba5aeef4dacab328d86c588602d789ae51668

--- RoundTripTests/bin/hudson.sh
+++ RoundTripTests/bin/hudson.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 #set env:
 #FIRTT_HOME 
-TIMESTAMP=`date '+%m%d%y_%H%M'`
 
 export FI_HOME=${FIRTT_HOME}/../FastInfoset
 export FIRTT_DATA=${FIRTT_HOME}/data
@@ -18,27 +17,11 @@ ant -f ${FIRTT_HOME}/build-without-nb.xml dist
 chmod 755 ${FIRTT_HOME}/bin/*
 chmod 755 ${FI_HOME}/bin/*
 
-#cd $FIRTT_DATA/xmlconf
-cd $FIRTT_DATA/xmlconf/eduni/errata-2e
-REPORT_TS=xmlts_${TIMESTAMP}.html
+cd $FIRTT_DATA/xmlconf
+REPORT_TS=xmlts.html
 ${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_TS}
 
-#cd ${FIRTT_DATA}/XBC
-cd ${FIRTT_DATA}/XBC/Docbook
-REPORT_XBC=xbc_${TIMESTAMP}.html
+cd ${FIRTT_DATA}/XBC
+REPORT_XBC=xbc.html
 ${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_XBC}
 
-mv ${FIRTT_DATA}/${REPORT_TS} ${RESULT_HOME}
-mv ${FIRTT_DATA}/${REPORT_XBC} ${RESULT_HOME}
-
-LOGFILE=${FIRTT_HOME}/report/roundtrip.html
-rm ${LOGFILE}
-
-echo ""                                                                 >> ${LOGFILE}
-echo "RountTripTests results"                                  >> ${LOGFILE}
-echo "-------------------------------------------"                      >> ${LOGFILE}
-echo "Test results for well-formed files from XML TS:"         >>${LOGFILE}
-echo "<a href=${REPORT_TS}>XMLTS result</>" >> ${LOGFILE}
-echo "-------------------------------------------"                      >> ${LOGFILE}
-echo "Test results for files from XBC test corpus:"         >>${LOGFILE}
-echo "<a href=${REPORT_XBC}>XBC result</>" >> ${LOGFILE}
