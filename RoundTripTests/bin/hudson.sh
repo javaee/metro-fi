e3a2ea752bc247b2292a93fcd295de3d4d1fc3da
joehw <joehw@localhost>
2006-05-04 19:41:26 +0000

    run a sub-set of tests
    svn path=/trunk/; revision=567
    
    
    Former-commit-id: bb3a088b4b11d207b60ac4c5483f18b167d988f2

--- RoundTripTests/bin/hudson.sh
+++ RoundTripTests/bin/hudson.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #set env:
 #FIRTT_HOME 
 
@@ -17,11 +17,11 @@ ant -f ${FIRTT_HOME}/build-without-nb.xml dist
 chmod 755 ${FIRTT_HOME}/bin/*
 chmod 755 ${FI_HOME}/bin/*
 
-cd $FIRTT_DATA/xmlconf
+cd $FIRTT_DATA/xmlconf/eduni/errata-2e
 REPORT_TS=xmlts.html
 ${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_TS}
 
-cd ${FIRTT_DATA}/XBC
+cd ${FIRTT_DATA}/XBC/Docbook
 REPORT_XBC=xbc.html
 ${FIRTT_HOME}/bin/allRoundtripTests.sh ${REPORT_XBC}
 
