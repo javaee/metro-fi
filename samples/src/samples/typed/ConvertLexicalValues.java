a551bcfa4b624eef189ccdd3bddb2b844cc83adc
sandoz <sandoz@localhost>
2006-06-20 16:56:38 +0000

    Included comments.
    svn path=/trunk/; revision=641
    
    
    Former-commit-id: b9ad48c10a60edf063c46d57a562c60ebbf230cf

--- samples/src/samples/typed/ConvertLexicalValues.java
+++ samples/src/samples/typed/ConvertLexicalValues.java
@@ -27,6 +27,8 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLFilterImpl;
 
 /**
+ * Example of converting lexical values to primitive types for encoding
+ * in binary form.
  *
  * @author Paul.Sandoz@Sun.Com
  */
@@ -43,25 +45,36 @@ public class ConvertLexicalValues {
             _attributes = attributes;
         }
         
+        /*
+         * Replace an the lexical value attribute with primitive data based
+         * on the set of XS data type associated with the local name of the
+         * attribute.
+         */
         private void replaceAttributeValue(Set<XSDataType> attributeTypes, 
                 int index, EncodingAlgorithmAttributesImpl atts) {
             if (attributeTypes == null) return;
             
             char[] ch = atts.getValue(index).toCharArray();
+            // Iterate through all possible types
             for (XSDataType dt : attributeTypes) {
                 try {
+                    // Switch on the type
                     switch(dt) {
                         case BASE64BINARY:
+                            // Convert the lexical value to byte[]
                             byte[] b = (byte[])BuiltInEncodingAlgorithmFactory.
                                     base64EncodingAlgorithm.
                                     convertFromCharacters(ch, 0, ch.length);
+                            // Replace the attribute value
                             atts.replaceWithAttributeAlgorithmData(index, 
                                     null, EncodingAlgorithmIndexes.BASE64, b);
                             return;
                         case FLOAT:
+                            // Convert the lexical value to float[]
                             float[] f = (float[])BuiltInEncodingAlgorithmFactory.
                                     floatEncodingAlgorithm.
                                     convertFromCharacters(ch, 0, ch.length);
+                            // Replace the attribute value
                             atts.replaceWithAttributeAlgorithmData(index, 
                                     null, EncodingAlgorithmIndexes.FLOAT, f);
                             return;
@@ -75,76 +88,113 @@ public class ConvertLexicalValues {
         
         public void startElement(String uri, String localName, String qName, 
                 Attributes atts) throws SAXException {
+            // Obtain the XS types associated with the local name of this 
+            // element
             _textContent = _elements.get(localName);
             
             for (int i = 0; i < atts.getLength(); i++) {
+                // Check if one or more attributes has a simple type associated with it
                 if (_attributes.containsKey(atts.getLocalName(i))) {
-                    // Copy attributes
+                    // Copy the attributes
                     final EncodingAlgorithmAttributesImpl eatts = new EncodingAlgorithmAttributesImpl(atts);
                     
+                    // Replace attribute lexical value with primitive types.
                     for (i = 0; i < atts.getLength(); i++) {
                         replaceAttributeValue(_attributes.get(atts.getLocalName(i)),
                                 i, eatts);
                     }
+                    
+                    // Replace attributes passed to parent
                     atts = eatts;
                     break;
                 }
             }
-            
+
+            // Pass event to parent
             super.startElement(uri, localName, qName, atts);
         }
         
         public void characters (char ch[], int start, int length)
             throws SAXException {
             if (_textContent == null) {
+                /*
+                 * If there are no types associated with element 
+                 * use the lexical value and defer to the parent.
+                 */
                 super.characters(ch, start, length);
                 return;
             }
 
+            // Iterate through all possible types
             for (XSDataType dt : _textContent) {
                 try {
+                    // Switch on the type
                     switch(dt) {
                         case BASE64BINARY:
+                            // Convert the lexical value to byte[]
                             byte[] b = (byte[])BuiltInEncodingAlgorithmFactory.
                                     base64EncodingAlgorithm.
                                     convertFromCharacters(ch, start, length);
+                            // Directly call the PrimitiveTypeContentHandler
                             ((PrimitiveTypeContentHandler)getContentHandler()).
                                     bytes(b, 0, b.length);
                             _textContent = null;
                             return;
                         case FLOAT:
+                            // Convert the lexical value to float[]
                             float[] f = (float[])BuiltInEncodingAlgorithmFactory.
                                     floatEncodingAlgorithm.
                                     convertFromCharacters(ch, start, length);
+                            // Directly call the PrimitiveTypeContentHandler
                             ((PrimitiveTypeContentHandler)getContentHandler()).
                                     floats(f, 0, f.length);
                             _textContent = null;
                             return;
                         default:
-                            super.characters(ch, start, length);
-                            _textContent = null;
-                            return;
                     }
                 } catch (Exception e) {
+                    // If conversion failed try the next type
                 }
             }
+    
+            /*
+             * If all conversions failed use the lexical value
+             * and defer to the parent.
+             */
+            super.characters(ch, start, length);
         }
     }
         
     public static void main(String[] args) throws Exception {
+        /**
+         * Process the schema to obtain a mapping of attribute/element
+         * local name to a set of XS data types.
+         */
         SchemaProcessor sp = new SchemaProcessor(new File(args[0]).toURL());
         sp.process();
         
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // Create an instance of the SAX document serializer
         SAXDocumentSerializer s = new SAXDocumentSerializer();
+        // Set the location of where the fast infoset document will be written
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         s.setOutputStream(baos);
         
+        /*
+         * Create a lexical filter whose parent is a JAXP XMLReader.
+         * Pass in the element/attribute local name to XS data type
+         * maps to the constructor.
+         */
         LexicalFilter lf = new LexicalFilter(sp.getElementToXSDataTypeMap(), 
                 sp.getAttributeToXSDataTypeMap());
         lf.setContentHandler(s);
         lf.setParent(getXMLReader());
+        // Parse the XML document to create a fast infoset document
         lf.parse(new InputSource(new FileInputStream(args[1])));
-        
+
+        /*
+         * Transform the fast infoset document to an XML document
+         * using a FastInfosetSource and the transform API.
+         */
         FastInfosetSource source = new FastInfosetSource(
                 new ByteArrayInputStream(baos.toByteArray()));
         StreamResult result = new StreamResult(System.out);
@@ -153,6 +203,11 @@ public class ConvertLexicalValues {
         t.transform(source, result);
         System.out.println();
         
+        /*
+         * Parse the fast infoset document using a SAX document
+         * parser. Register a PrimitiveContentHandler to access
+         * binary data as arrays of primitives.
+         */
         SAXDocumentParser p = new SAXDocumentParser();
         FastInfosetDefaultHandler h = new FastInfosetDefaultHandler() {
             public void bytes(byte[] b, int start, int length) 
