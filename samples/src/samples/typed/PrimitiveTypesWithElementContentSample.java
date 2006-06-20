4e3c027789e1356164e03e6639481bd628633a15
sandoz <sandoz@localhost>
2006-06-20 15:38:57 +0000

    Updated to include the use of encoding algorithms with attributes.
    svn path=/trunk/; revision=633
    
    
    Former-commit-id: 4efdc89b4c190491254174f39ef2ff4edf80b744

--- samples/src/samples/typed/PrimitiveTypesWithElementContentSample.java
+++ samples/src/samples/typed/PrimitiveTypesWithElementContentSample.java
@@ -42,9 +42,13 @@ import com.sun.xml.fastinfoset.sax.SAXDocumentSerializer;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import org.jvnet.fastinfoset.EncodingAlgorithmIndexes;
+import org.jvnet.fastinfoset.sax.EncodingAlgorithmAttributes;
 import org.jvnet.fastinfoset.sax.helpers.FastInfosetDefaultHandler;
 import org.jvnet.fastinfoset.sax.FastInfosetReader;
 import org.jvnet.fastinfoset.sax.FastInfosetWriter;
+import org.jvnet.fastinfoset.sax.helpers.EncodingAlgorithmAttributesImpl;
+import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -70,21 +74,38 @@ public class PrimitiveTypesWithElementContentSample {
         // Set the OutputStream to write the document to
         fiw.setOutputStream(baos);
         
+        // Create an attributes holder that can hold algorithm data
+        EncodingAlgorithmAttributesImpl atts = new EncodingAlgorithmAttributesImpl();
         // Serialize the infoset
         fiw.startDocument();
             fiw.startElement("", "root", "root", null);
-                fiw.startElement("", "bytes", "bytes", null);
-                    byte[] bytes = {1, 2, 3, 4};
+                byte[] bytes = {1, 2, 3, 4};
+                atts.clear();
+                // Add an attirbute value that holds byte[]
+                atts.addAttributeWithBuiltInAlgorithmData("", "value", "value", 
+                        EncodingAlgorithmIndexes.BASE64, bytes);
+                fiw.startElement("", "bytes", "bytes", atts);
                     // Write an array of bytes using the PrimitiveTypeContentHandler
                     fiw.bytes(bytes, 0, bytes.length);
                 fiw.endElement("", "bytes", "bytes");
-                fiw.startElement("", "integers", "integers", null);
-                    int[] ints = {1, 2, 3, 4};
+                
+                int[] ints = {1, 2, 3, 4};
+                atts.clear();
+                // Add an attirbute value that holds int[]
+                atts.addAttributeWithBuiltInAlgorithmData("", "value", "value", 
+                        EncodingAlgorithmIndexes.INT, ints);
+                fiw.startElement("", "integers", "integers", atts);
                     // Write an array of integers using the PrimitiveTypeContentHandler
                     fiw.ints(ints, 0, ints.length);
                 fiw.endElement("", "integers", "integers");
-                fiw.startElement("", "floats", "floats", null);
-                    float[] floats = {1.0f, 2.0f, 3.0f, 4.0f};
+                
+                
+                float[] floats = {1.0f, 2.0f, 3.0f, 4.0f};
+                atts.clear();
+                // Add an attirbute value that holds float[]
+                atts.addAttributeWithBuiltInAlgorithmData("", "value", "value", 
+                        EncodingAlgorithmIndexes.FLOAT, floats);
+                fiw.startElement("", "floats", "floats", atts);
                     // Write an array of floats using the PrimitiveTypeContentHandler
                     fiw.floats(floats, 0, floats.length);
                 fiw.endElement("", "floats", "floats");
@@ -104,8 +125,57 @@ public class PrimitiveTypesWithElementContentSample {
         // Create a default FI handler that will receive events for primitive types
         FastInfosetDefaultHandler h = new FastInfosetDefaultHandler() {
             
+            public void startElement (String uri, String localName, String qName, 
+                    Attributes attributes) throws SAXException {
+                // Check if the attributes implements the EncodingAlgorithmAttributes interface
+                if (attributes instanceof EncodingAlgorithmAttributes) {
+                    EncodingAlgorithmAttributes a = (EncodingAlgorithmAttributes)attributes;
+                    
+                    // Iterate through the attributes
+                    for (int index = 0; index < a.getLength(); index++) {
+                        // Get the algorithm data
+                        Object data = a.getAlgorithmData(index);
+                        // If there is binary data then the returned valus will not be null
+                        if (data != null) {
+                            // Switch on the algorithm used
+                            switch (a.getAlgorithmIndex(index)) {
+                                case EncodingAlgorithmIndexes.BASE64:
+                                    System.out.print("Attribute value as bytes: ");
+                                    byte[] bs = (byte[])data;
+                                    for (int i = 0; i < bs.length; i++) {
+                                        if (i > 0) System.out.print(" ");
+                                        System.out.print(bs[i]);
+                                    }
+                                    System.out.println();
+                                    break;
+                                case EncodingAlgorithmIndexes.INT:
+                                    System.out.print("Attribute value as ints: ");
+                                    int[] is = (int[])data;
+                                    for (int i = 0; i < is.length; i++) {
+                                        if (i > 0) System.out.print(" ");
+                                        System.out.print(is[i]);
+                                    }
+                                    System.out.println();
+                                    break;
+                                case EncodingAlgorithmIndexes.FLOAT:
+                                    System.out.print("Attribute value as floats: ");
+                                    float[] fs = (float[])data;
+                                    for (int i = 0; i < fs.length; i++) {
+                                        if (i > 0) System.out.print(" ");
+                                        System.out.print(fs[i]);
+                                    }
+                                    System.out.println();
+                                    break;
+                                default:
+                            }
+                        }
+                    }
+                }
+            }
+    
             // Recieve events for an array of byte[]
             public void bytes(byte[] bs, int start, int length) throws SAXException {
+                System.out.print("element content as bytes: ");
                 for (int i = 0; i < length; i++) {
                     if (i > 0) System.out.print(" ");
                     System.out.print(bs[start + i]);
@@ -115,6 +185,7 @@ public class PrimitiveTypesWithElementContentSample {
 
             // Recieve events for an array of int[]
             public void ints(int[] is, int start, int length) throws SAXException {
+                System.out.print("element content as ints: ");
                 for (int i = 0; i < length; i++) {
                     if (i > 0) System.out.print(" ");
                     System.out.print(is[start + i]);
@@ -124,6 +195,7 @@ public class PrimitiveTypesWithElementContentSample {
 
             // Recieve events for an array of float[]
             public void floats(float[] fs, int start, int length) throws SAXException {
+                System.out.print("element content as floats: ");
                 for (int i = 0; i < length; i++) {
                     if (i > 0) System.out.print(" ");
                     System.out.print(fs[start + i]);
@@ -155,9 +227,16 @@ public class PrimitiveTypesWithElementContentSample {
 
         // Create a handler that will receive standard SAX events
         ContentHandler h = new DefaultHandler() {
+            public void startElement (String uri, String localName, String qName, 
+                    Attributes attributes) throws SAXException {
+                for (int index = 0; index < attributes.getLength(); index++) {
+                    System.out.println("attribute value as text: " + attributes.getValue(index));
+                }
+            }
+            
             public void characters (char ch[], int start, int length) throws SAXException {
                 String s = new String(ch, start, length);
-                System.out.println(s);
+                System.out.println("element content as text: " + s);
             }
         };
         
